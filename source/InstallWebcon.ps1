4644f1da748de9a9f2d972d4f690776e9cb6b441
Tomasz Dłuski <mr.toumash@gmail.com>
2022-02-24 19:06:52 +0100

    add webcon bpssearch and studio .msi installers

--- source/InstallWebcon.ps1
+++ source/InstallWebcon.ps1
@@ -2,24 +2,27 @@
 $Folder = 'C:\install\WebconBPS'
 "Test to see if folder [$Folder]  exists"
 if (!(Test-Path -Path $Folder)) {
-    "[*] START: Expand Archive, because its not extracted"
+    "WebconBPS.zip extracting..."
     Expand-Archive WebconBPS.zip -DestinationPath C:\install
-    "[*] DONE: WebconBPS.zip"
+    "WebconBPS.zip"
 }
 
 # $setupFile = Get-ChildItem -Recurse -Path $Folder | Where-Object { $_.Name -match 'setup.exe' } | Select-Object Fullname  -First 1
 # & $setupFile[0].FullName --silent --sqlServer . --cfgDb --solrAdminUser root --solrAdminPassword root
 
-$serviceInstaller = Get-ChildItem -Recurse -Path $Folder | Where-Object { $_.Name -match 'WebCon.WorkFlow.Service.Installer.msi' } | Select-Object Fullname  -First 1
-"[*] DONE: WebCon.WorkFLow.Service.Installer.msi"
-msiexec /i (Resolve-Path $serviceInstaller[0].FullName) /QN /L*V C:\Temp\msilog.log /norestart
+$workflowService = Get-ChildItem -Recurse -Path $Folder | Where-Object { $_.Name -match 'WebCon.WorkFlow.Service.Installer.msi' } | Select-Object Fullname  -First 1
+"WebCon.WorkFlow.Service.Installer.msi"
+msiexec /i (Resolve-Path $workflowService[0].FullName) /QN /L*V C:\Temp\msilog.log /norestart
 
+$bpsSearch = Get-ChildItem -Recurse -Path $Folder | Where-Object { $_.Name -match 'WebCon.WorkFlow.Service.Installer.msi' } | Select-Object Fullname  -First 1
+"WebCon.BPSSearchServer.msi"
+msiexec /i (Resolve-Path $bpsSearch[0].FullName) /QN /L*V C:\Temp\msilog.log /norestart
 
 $bpsPortal = Get-ChildItem -Recurse -Path $Folder | Where-Object { $_.Name -match 'WebCon.BPSPortal.Standalone.msi' } | Select-Object Fullname  -First 1
-"[*] DONE: WebCon.BPSPortal.Standalone.msi"
+"WebCon.BPSPortal.Standalone.msi"
 msiexec /i (Resolve-Path $bpsPortal[0].FullName) /QN /L*V C:\Temp\msilog.log /norestart
 
 
 $studio = Get-ChildItem -Recurse -Path $Folder | Where-Object { $_.Name -match 'StudioInstall.bat' } | Select-Object Fullname  -First 1
-"[*] DONE: StudioInstall.bat"
-& $bpsPortal[0].FullName
+"StudioInstall.bat"
+& $studio[0].FullName
